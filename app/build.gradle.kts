01b756d920ea714de08e5e6ed283ded97ff43486
AnkanaPanja <ankana.panja05@gmail.com>
2024-09-18 09:43:02 +0530

    message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.airbnb.android:lottie:5.2.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
