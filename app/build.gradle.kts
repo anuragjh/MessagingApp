6cb7db3cd3823e7b14fbff64a800cf659c302a95
anuragjh <anuragjhav51@gmail.com>
2024-09-18 09:40:42 +0530

    LOGIN XML SETUP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
 
+    //lottieAnimation dependency
+    implementation("com.airbnb.android:lottie:5.2.0")
+
     // Retrofit and Gson Converter
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
